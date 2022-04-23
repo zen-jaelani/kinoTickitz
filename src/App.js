46149324d454d59e4b163e504c1be80215ef8dc2
Qxtlp <xi.tulop@yandex.com>
2022-04-23 21:24:04 +0700

    add home path

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/home/index";
 import BasicCounter from "./pages/basic/Counter/classComponent";
 import BasicReact from "./pages/basic/React";
 import Register from "./pages/auth/register";
@@ -8,6 +9,7 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path="" element={<Home />} />
         <Route path="basic/counter" element={<BasicCounter />} />
         <Route path="basic/react" element={<BasicReact />} />
         <Route path="auth/register" element={<Register />} />

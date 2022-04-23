73b5e448db66130675af569515f3b38b35f54235
Qxtlp <xi.tulop@yandex.com>
2022-04-23 21:23:46 +0700

    add bootstrap icon & script

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap-icons/font/bootstrap-icons.css";
+import "../node_modules/bootstrap/dist/js/bootstrap.min.js";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 

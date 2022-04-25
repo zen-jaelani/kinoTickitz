ef15aeaf57ddde9191de7d498d6b62d53c82cd30
Qxtlp <xi.tulop@yandex.com>
2022-04-25 21:32:08 +0700

    add movie card component

---
+++ src/components/cards/movie.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+import styles from "./movie.module.css";
+
+function movie(props) {
+  const { id, name, category, image } = props.data;
+
+  return (
+    <>
+      <div
+        className={
+          props.type == "showing"
+            ? `card ${styles.cardShowing} h-100 ${styles.card}`
+            : `card ${styles.cardUpcoming} h-100 ${styles.card}`
+        }
+      >
+        <img
+          src={
+            image
+              ? `https://res.cloudinary.com/qxtlp/image/upload/v1649062140/${image}`
+              : "https://res.cloudinary.com/qxtlp/image/upload/v1650786142/default-movie.png"
+          }
+          alt=""
+          className=""
+        />
+        <div className={props.type == "showing" ? `${styles.cardPopup}` : "d-block"}>
+          <div className="card-body text-center text-wrap p-0 py-3">
+            <h5 className={`card-title ${styles.cardTitle}`}>{name}</h5>
+            <p className="card-text text-secondary">{category} </p>
+          </div>
+          <button className="btn btn-primary btn-sm" onClick={() => props.handleDetail(id)}>
+            Details
+          </button>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default movie;

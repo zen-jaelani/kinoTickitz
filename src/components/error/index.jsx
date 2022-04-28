2ff4fe94f164ce4e14f68518abffde218458afda
Qxtlp <xi.tulop@yandex.com>
2022-04-28 08:13:50 +0700

    add error massage component

---
+++ src/components/error/index.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+function Error(props) {
+  const { status, statusText } = props.data ? props.data : [];
+  return (
+    <div>
+      <div className="d-flex justify-content-center align-items-center" style={{ height: "50vh" }}>
+        <h1 className="me-3 pe-3 align-top border-right inline-block align-content-center border-end">
+          {status ? status : "400"}
+        </h1>
+        <div className="inline-block align-middle">
+          <h2 className="font-weight-normal lead">{statusText ? statusText : "Bad Request"} </h2>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Error;

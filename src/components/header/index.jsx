4c360b47f1660b133d9118c02eab946b9a8ad7a6
Qxtlp <xi.tulop@yandex.com>
2022-04-28 09:28:09 +0700

    add search functionality

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -1,8 +1,49 @@
-import React from "react";
-import styles from "./header.module.css";
+import React, { useState, useEffect } from "react";
 import { Link, useNavigate } from "react-router-dom";
+import styles from "./header.module.css";
+import axios from "../../utils/axios";
 
 function Header() {
+  const [user, setUser] = useState({});
+  const [searchState, setSearchState] = useState(false);
+  const [searchValue, setSearchValue] = useState("");
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    getData();
+  }, []);
+
+  const getData = async () => {
+    try {
+      if (localStorage.getItem("id")) {
+        const result = await axios.get(`user/${localStorage.getItem("id")}`);
+        setUser(result.data.data);
+      }
+    } catch (error) {
+      console.log(error, error.response);
+    }
+  };
+  console.log(user);
+
+  const showSearch = () => {
+    if (!searchState) {
+      setSearchState(true);
+    } else {
+      setSearchState(false);
+    }
+  };
+  const search = (event) => {
+    setSearchValue(event.target.value);
+  };
+
+  const enter = (event) => {
+    console.log(event.key);
+    console.log(searchValue);
+    if (event.key == "Enter") {
+      navigate("/all", { state: searchValue });
+    }
+  };
+
   return (
     <>
       <header>
@@ -39,28 +80,52 @@ function Header() {
                     </Link>
                   </button>
                 </ul>
-                <ul className="navbar-nav w-100 align-items-center justify-content-end px-md-5">
-                  <form className="form-inline pt-5 pt-md-0 order-0 pe-md-4">
-                    <div className="md-form my-0">
-                      <div className="input-group">
-                        <input
-                          className="form-control mr-sm-2 d-block d-lg-none"
-                          type="text"
-                          placeholder="Search"
-                          aria-label="Search"
-                        />
-                        <button className="btn d-none d-lg-block">
-                          <i className="bi bi-search"></i>
-                        </button>
+
+                <ul
+                  className={`navbar-nav w-100 align-items-center justify-content-end px-md-5 ${
+                    user.id ? "d-flex" : "d-none"
+                  }`}
+                >
+                  {/* <form className="form-inline pt-5 pt-md-0 order-0 pe-md-4"> */}
+                  <div className="md-form my-0">
+                    <div className="input-group">
+                      <input
+                        className={`form-control mr-sm-2 d-block  ${
+                          searchState ? "d-lg-block" : "d-lg-none"
+                        }`}
+                        type="text"
+                        placeholder="Search"
+                        onChange={search}
+                        onKeyDownCapture={enter}
+                      />
+                      <div className="btn d-none d-lg-block" onClick={showSearch}>
+                        <i className="bi bi-search"></i>
                       </div>
                     </div>
-                  </form>
+                  </div>
+                  {/* </form> */}
                   <img
-                    src={require("../../assets/image/afef0e154500795b18c0928f62adb3d1.jpg")}
+                    src={
+                      user.image
+                        ? `${process.env.REACT_APP_IMG_URL}${user.image}`
+                        : `${process.env.REACT_APP_IMG_URL}default-profile.jpg`
+                    }
                     alt=""
                     className={`d-none d-lg-block ${styles.profileImage}`}
                   />
                 </ul>
+
+                <ul className={`navbar-nav order-3 ${!user.id ? "d-block" : "d-none"}`}>
+                  <button
+                    className={`btn btn-block btn-lg text-white w-100 ${styles.mainBg}`}
+                    style={{ width: "100px" }}
+                  >
+                    <Link to="/auth/login" className="text-white">
+                      Sign In
+                    </Link>
+                  </button>
+                </ul>
+
                 <span className="mx-auto pt-4 order-last d-block d-md-none">
                   © 2020 Tickitz. All Rights Reserved.
                 </span>
